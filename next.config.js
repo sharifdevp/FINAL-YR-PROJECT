b313cb2fec2dfafaf1e5d29df3912eb8a6a51159
Tibemanya Sharif <tibemanyasharifa@gmail.com>
2024-06-03 15:10:56 +0300

    added-sms-email-successfully-working

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
+// console.log(process.env.DATABASE_URL); // This should print the DATABASE_URL value
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-//module.exports = nextConfig
 module.exports = {
   reactStrictMode: true,
   swcMinify: false,
