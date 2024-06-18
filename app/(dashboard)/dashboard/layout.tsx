c036bae7133f562325ad4b0bec9465cecb8c087b
Tibemanya Sharif <tibemanyasharifa@gmail.com>
2024-06-18 19:10:56 +0300

    session uppdate2

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/Common/Header';
 import SideBar from '@/components/Common/SideBar';
-import { getCurrentUser } from '@/lib/sessions/userSession';
+import { getCurrentUser } from '@/lib/sessions/session';
 import { User } from '@prisma/client';
 
 export default async function DashLayout({
